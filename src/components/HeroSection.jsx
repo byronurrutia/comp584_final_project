36ddf76163040b18e7c5a9047bd4513bf3ca636d
byronurrutia <b.urrutia@me.com>
2022-11-12 15:30:18 -0800

    margin fixing

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <div className="hero--section text-center d-flex flex-column justify-content-center">
       <h1 className="display-4 fw-bold pb-5 mt-5">Coding Themed Clothing</h1>
-      <div className="col-7 mx-auto">
+      <div className="col-7 mx-auto mb-5">
         <div className="gap-2 d-xl-flex d-grid justify-content-xl-center">
           <Button variant="light" size="large" className="px-xl-4 shadow">
             Outerwear
