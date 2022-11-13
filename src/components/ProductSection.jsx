d0796edb36e59436aaa62c69299de4196f478a7c
byronurrutia <b.urrutia@me.com>
2022-11-13 11:25:55 -0800

    product section grid spacing changes

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -4,9 +4,9 @@ import tShirtImage from "../assets/tshirt.jpg";
 export default function ProductSection() {
   return (
     <div className="product--section">
-      <Container className="px-5">
-        <Row className="row-cols-2 row-cols-xl-4">
-          <Col className="mb-4">
+      <Container>
+        <Row className="row-cols-2 row-cols-xl-4 gy-2 gx-4 px-0 px-md-5">
+          <Col>
             <Card style={{ width: "100%" }} className="shadow grow">
               <Card.Img variant="top" src={tShirtImage} />
               <Card.Body>
@@ -15,7 +15,7 @@ export default function ProductSection() {
               </Card.Body>
             </Card>
           </Col>
-          <Col className="mb-4">
+          <Col>
             <Card style={{ width: "100%" }} className="shadow grow">
               <Card.Img variant="top" src={tShirtImage} />
               <Card.Body>
