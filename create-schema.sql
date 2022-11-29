69b05c8f5745b66b1a91a7b434e9f41375447822
Junhe-Chen-9 <contact@junhechen.com>
2022-11-28 17:11:37 -0800

    creating mysql schema

---
+++ create-schema.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Schema foody-app
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `e-commerce-app-584`;
+CREATE SCHEMA `e-commerce-app-584`;
+USE `e-commerce-app-584` ;
